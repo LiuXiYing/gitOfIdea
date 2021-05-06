6181cdd708f1cd0c50257fd7135ca27a342247be
Enter <2671642981@qq.com>
2021-05-06 11:51:27 +0800

    "version6"

--- src/main/java/com/lxy/Git.java
+++ src/main/java/com/lxy/Git.java
@@ -14,6 +14,8 @@ public class Git {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 
 }
